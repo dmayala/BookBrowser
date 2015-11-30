550360b2cb157b00e2256bcfeb3adbd2fe62bd73
Daniel Ayala <dmayala001@gmail.com>
2015-11-29 21:18:37 -0800

    Fetch book list from Google Books API

--- ResultsScreen.js
+++ ResultsScreen.js
@@ -1,20 +1,84 @@
 'use strict';
 
 import React from 'react-native';
-const { View, Text, StyleSheet } = React;
+const { View, ListView, Text, Image, StyleSheet } = React;
+
+let buildUrl = (q) => {
+  return `https:/\/\www.googleapis.com/books/v1/volumes?q=${ encodeURIComponent(q) }&langRestrict=en&maxResults=40`;
+};
 
 class ResultsScreen extends React.Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    };
+  }
+
+  componentDidMount() {
+    this.fetchResults(this.props.searchPhrase);
+  }
+
+  fetchResults(searchPhrase) {
+    return fetch(buildUrl(searchPhrase))
+            .then(response => response.json())
+            .then(jsonData => {
+              this.setState({ 
+                isLoading: false,
+                dataSource: this.state.dataSource.cloneWithRows(jsonData.items)
+              });
+            })
+            .catch(err => console.dir(err)); 
+  }
+
+  renderLoadingMessage() {
     return (
       <View style={styles.container}>
         <Text style={styles.label}>
-          This is the results screen
+          Searching for "{this.props.searchPhrase}".
         </Text>
         <Text style={styles.label}>
-          You search for: {this.props.searchPhrase}
+          Please wait...
         </Text>
       </View>
     );
+  }
+
+  renderBook(book) {
+    return (
+      <View style={styles.row}>
+        <Image
+          style={styles.thumbnail}
+          source={{uri: book.volumeInfo.imageLinks.smallThumbnail}}
+          />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>
+           {book.volumeInfo.title}
+          </Text>
+          <Text style={styles.subtitle}>
+            {book.volumeInfo.subtitle}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+  renderResults() {
+    return (
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderBook}
+          style={styles.listView}
+          />
+    );
+  }
+
+  render() {
+    return this.state.isLoading ? this.renderLoadingMessage() : this.renderResults();
   }
 }
 
@@ -28,6 +92,34 @@ let styles = StyleSheet.create({
   },
   label: {
     fontSize: 24,
+    fontWeight: 'normal',
+    color: '#fff'
+  },
+  listView: {},
+  rightContainer: {
+    flex: 1
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5AC8FA',
+    paddingRight: 20,
+    marginTop: 1
+  },
+  thumbnail: {
+    width: 70,
+    height: 108,
+    marginRight: 16
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  subtitle: {
+    fontSize: 16,
     fontWeight: 'normal',
     color: '#fff'
   }
